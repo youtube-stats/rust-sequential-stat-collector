4eabab325a773ed2d5b64027ef121a37710f4e7e
Salvador Guzman <guzmansalv@gmail.com>
2019-05-21 09:40:37 -0700

    bad inner loop

--- src/main.rs
+++ src/main.rs
@@ -73,90 +73,92 @@ fn main() {
         postgres::Connection::connect(params, tls).unwrap();
 
     let key: String = std::env::var("YOUTUBE_KEY").unwrap();
+    let mut offset: u32 = 0;
 
     loop {
-        let mut offset: u32 = 0;
         let rows: postgres::rows::Rows = conn.query(QUERY, &[&offset]).unwrap();
 
-        loop {
-            let mut hash: std::collections::HashMap<String, String> = HashMap::new();
-            for row in &rows {
-                let k: String = row.get(1);
-                let v: i32 = row.get(0);
-                let v: String = v.to_string();
+        let mut hash: std::collections::HashMap<String, String> = HashMap::new();
+        for row in &rows {
+            let k: String = row.get(1);
+            let v: i32 = row.get(0);
+            let v: String = v.to_string();
 
-                hash.insert(k, v);
-            }
-
-            let mut vec_id: Vec<String> = Vec::new();
-            for value in hash.keys().clone() {
-                vec_id.push(value.to_string());
-            }
-
-            let ids: String = vec_id.join(",");
-            let url: String = format!("https://www.googleapis.com/youtube/v3/channels?part=statistics&key={}&id={}", key, ids);
-
-            let mut resp: reqwest::Response = match reqwest::get(url.as_str()) {
-                Ok(resp) => resp,
-                Err(e) => {
-                    eprintln!("{}", e.to_string());
-                    continue
-                }
-            };
-
-            let body: String = match resp.text() {
-                Ok(text) => text,
-                Err(e) => {
-                    eprintln!("{}", e.to_string());
-                    continue
-                }
-            };
-
-            let response: YoutubeResponseType = match serde_json::from_str(body.as_str()) {
-                Ok(text) => text,
-                Err(e) => {
-                    eprintln!("{}", e.to_string());
-                    continue
-                }
-            };
-
-            for item in response.items {
-                let channel_id: &String = match hash.get(item.id.as_str()) {
-                    Some(text) => text,
-                    None => {
-                        eprintln!("Found no value for key {}", item.id);
-                        continue
-                    }
-                };
-
-                println!("{} {} {} {} {}",
-                         item.id,
-                         channel_id,
-                         item.statistics.subscriberCount,
-                         item.statistics.viewCount,
-                         item.statistics.videoCount);
-
-                let query: String =
-                    format!("INSERT INTO youtube.stats.metrics (channel_id, subs, views, videos) VALUES ({}, {}, {}, {})",
-                            channel_id,
-                            item.statistics.subscriberCount,
-                            item.statistics.viewCount,
-                            item.statistics.videoCount);
-
-                let n: u64 = match conn.execute(query.as_str(), &[]) {
-                    Ok(size) => size,
-                    Err(e) => {
-                        eprintln!("{}", e.to_string());
-                        continue
-                    }
-                };
-
-                if n != 1 {
-                    eprintln!("Row did not insert correctly");
-                }
-            }
+            hash.insert(k, v);
         }
 
-        offset += 50;
+        if hash.len() == 50 {
+            offset += 50;
+        } else {
+            offset = 0;
+        }
+
+        let mut vec_id: Vec<String> = Vec::new();
+        for value in hash.keys().clone() {
+            vec_id.push(value.to_string());
+        }
+
+        let ids: String = vec_id.join(",");
+        let url: String = format!("https://www.googleapis.com/youtube/v3/channels?part=statistics&key={}&id={}", key, ids);
+
+        let mut resp: reqwest::Response = match reqwest::get(url.as_str()) {
+            Ok(resp) => resp,
+            Err(e) => {
+                eprintln!("{}", e.to_string());
+                continue
+            }
+        };
+
+        let body: String = match resp.text() {
+            Ok(text) => text,
+            Err(e) => {
+                eprintln!("{}", e.to_string());
+                continue
+            }
+        };
+
+        let response: YoutubeResponseType = match serde_json::from_str(body.as_str()) {
+            Ok(text) => text,
+            Err(e) => {
+                eprintln!("{}", e.to_string());
+                continue
+            }
+        };
+
+        for item in response.items {
+            let channel_id: &String = match hash.get(item.id.as_str()) {
+                Some(text) => text,
+                None => {
+                    eprintln!("Found no value for key {}", item.id);
+                    continue
+                }
+            };
+
+            println!("{} {} {} {} {}",
+                     item.id,
+                     channel_id,
+                     item.statistics.subscriberCount,
+                     item.statistics.viewCount,
+                     item.statistics.videoCount);
+
+            let query: String =
+                format!("INSERT INTO youtube.stats.metrics (channel_id, subs, views, videos) VALUES ({}, {}, {}, {})",
+                        channel_id,
+                        item.statistics.subscriberCount,
+                        item.statistics.viewCount,
+                        item.statistics.videoCount);
+
+            let n: u64 = match conn.execute(query.as_str(), &[]) {
+                Ok(size) => size,
+                Err(e) => {
+                    eprintln!("{}", e.to_string());
+                    continue
+                }
+            };
+
+            if n != 1 {
+                eprintln!("Row did not insert correctly");
+            }
+        }
     }
 }
